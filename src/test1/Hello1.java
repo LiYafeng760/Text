7f28b2e86660af71d839cfc6fd7a394ad58afd46
LiYafeng <Liyafeng@me.com>
2015-01-23 15:11:58 +0800

    12321

--- src/test1/Hello1.java
+++ src/test1/Hello1.java
@@ -5,6 +5,7 @@ public class Hello1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello1");
+       System.out.println("Hello22222222222222");
 	}
 
 }
